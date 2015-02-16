33ae4759fb49f35b20f950583110d1c0a8eb63ae
Rupert Eder <rupert.eder@redvalley-software.com>
2015-02-16 22:14:10 +0100

    added initial project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# redcloud
-The aim of this project is to create a private cloud software (with calendars, chat, contacts ...) using modern frameworks and technologies.
---
+++ src/main/java/com/redvalley/redcloud/domain/Identity.java
@@ -0,0 +1,15 @@
+package com.redvalley.redcloud.domain;
+
+
+import java.util.Collection;
+
+public class Identity {
+
+    private long id;
+    private Collection<Address> addresses;
+
+    
+
+
+
+}
